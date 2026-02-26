8c195c97a1a93ddb4fbc6898bdf048f6d4cfa0f7
Sahana MB <sahanamb2005@gmail.com>
2026-02-26 13:01:53 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,3 +1,5 @@
-public class{
-  public static void main(String args[])
-    System.out.print("hello world');}}
+public app.java{
+  public static void main(String args[]){
+    System.out.print("hello world');
+  }
+}
