cdedc5875c6b5fdb1e2d1f7bc76dc474b20f5c82
Sahana MB <sahanamb2005@gmail.com>
2026-02-28 12:22:41 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,9 +1,5 @@
-public class app{
+public class app {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
-        int sum = num1 + num2;
-
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("Hello, World!");
     }
 }
