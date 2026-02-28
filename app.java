03edc7adb1469be67f1107b778d437a08cd486de
Sahana MB <sahanamb2005@gmail.com>
2026-02-28 12:17:11 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,13 +1,8 @@
-public class app {
+public class app{
     public static void main(String[] args) {
-        // Declare and initialize two integer variables
         int num1 = 10;
         int num2 = 20;
-        
-        // Add the numbers and store the result in a 'sum' variable
-        int sum = num1 + num2;
-        
-        // Print the result to the console
-        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        int sum = num1 + num2; // Using the '+' operator
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
